2848e77378ee174a0ba2ccf251cbb9a018f02908
HassanBouighamdane <hassanbouighamdane2002@gmail.com>
2024-02-19 20:51:08 +0100

    First commit

---
+++ ListingPosting-microservice/src/main/java/com/example/listingpostingmicroservice/ListingPostingMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.listingpostingmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListingPostingMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ListingPostingMicroserviceApplication.class, args);
+    }
+
+}
