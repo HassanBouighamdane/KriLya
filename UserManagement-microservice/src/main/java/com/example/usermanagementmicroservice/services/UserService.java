cc4ff94f1b3d92ec9950521060f5fef49959967b
mehdiboutoumit <mehdiboutoumit03@gmail.com>
2024-02-21 16:35:39 +0100

    User crud

---
+++ UserManagement-microservice/src/main/java/com/example/usermanagementmicroservice/services/UserService.java
@@ -0,0 +1,48 @@
+package com.example.usermanagementmicroservice.services;
+
+
+
+import com.example.usermanagementmicroservice.models.User;
+import com.example.usermanagementmicroservice.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(String id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User updateUser(String id, User userDetails) {
+        User user = userRepository.findById(id).orElse(null);
+        if (user != null) {
+            user.setUsername(userDetails.getUsername());
+            user.setEmail(userDetails.getEmail());
+            user.setPassword(userDetails.getPassword());
+            return userRepository.save(user);
+        }
+        return null;
+    }
+
+    public void deleteUser(String id) {
+        userRepository.deleteById(id);
+    }
+}
+
