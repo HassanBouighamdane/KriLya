6d774f009a1046fddc82da43c9ada159aaaaad6e
mehdiboutoumit <mehdiboutoumit03@gmail.com>
2024-02-21 23:37:00 +0100

    Define dynamic routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Signup from './pages/Signup';
 
 function App() {
+
+  const routes = [
+    //auth
+    {url : "signup", component: Signup},
+  ]
+
   return (
     <div className="App">
-     
+     <Router basename="/" >
+        <Switch>
+        <Route>
+          <div id="" className="" style={{minHeight :"100vh", display: "flex", flexDirection: "column"}}>
+            {/* <Nav /> */}
+            <div className="content-body" style={{ flex: 1 , marginBottom : 2}}>
+              <div className="container-fluid" style={{ flex: 1 , marginBottom : 2}}>
+                <Switch>
+                  {routes.map((data, i) => (
+                    <Route
+                      key={i}
+                      exact
+                      path={`/${data.url}`}
+                      component={data.component}
+                    />
+                  ))}
+                   <Route path="*">
+            {/* <Error404 /> */}
+          </Route>
+                </Switch>
+              </div>
+            </div>
+            <div style={{  marginTop : 100}}>
+            {/* <Footer /> */}
+           
+            </div>
+          </div>
+        </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
