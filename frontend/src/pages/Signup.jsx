233449dc3df4f32c0242734d4eeb940d144967f0
mehdiboutoumit <mehdiboutoumit03@gmail.com>
2024-02-21 23:44:13 +0100

    Form version 1

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,8 +1,64 @@
 import React from 'react'
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 
 function Signup() {
   return (
-    <div>S</div>
+    <div>
+    <h1>Registration Form</h1>
+    <Formik
+      initialValues={{
+        username: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+      }}
+      validationSchema={Yup.object({
+        username: Yup.string()
+          .min(3, 'Username must be at least 3 characters')
+          .required('Username is required'),
+        email: Yup.string()
+          .email('Invalid email address')
+          .required('Email is required'),
+        password: Yup.string()
+          .min(6, 'Password must be at least 6 characters')
+          .required('Password is required'),
+        confirmPassword: Yup.string()
+          .oneOf([Yup.ref('password'), null], 'Passwords must match')
+          .required('Confirm Password is required'),
+      })}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      <Form>
+        <div>
+          <label htmlFor="username">Username</label>
+          <Field type="text" name="username" />
+          <ErrorMessage name="username" />
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <Field type="email" name="email" />
+          <ErrorMessage name="email" />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <Field type="password" name="password" />
+          <ErrorMessage name="password" />
+        </div>
+        <div>
+          <label htmlFor="confirmPassword">Confirm Password</label>
+          <Field type="password" name="confirmPassword" />
+          <ErrorMessage name="confirmPassword" />
+        </div>
+        <button type="submit">Submit</button>
+      </Form>
+    </Formik>
+  </div>
   )
 }
 
