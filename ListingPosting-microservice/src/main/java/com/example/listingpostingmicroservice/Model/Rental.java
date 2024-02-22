31bc91a93a14a52e5327b8e1b911ee1037b66280
Ben Hadin Maryem <119045781+maryembe@users.noreply.github.com>
2024-02-22 15:38:45 +0100

    search categories && sortings

--- ListingPosting-microservice/src/main/java/com/example/listingpostingmicroservice/Model/Rental.java
+++ ListingPosting-microservice/src/main/java/com/example/listingpostingmicroservice/Model/Rental.java
@@ -1,24 +1,39 @@
 package com.example.listingpostingmicroservice.Model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Getter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+
 import java.util.List;
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
+
 @Document(collection = "rentals")
 public class Rental {
 
     @Id
     private String id;
+    @Getter
     private String title;
+    @Getter
     private String description;
+    @Getter
     private double pricePerDay;
+    @Getter
     private boolean availability;
+    @Getter
     private String location;
+    @Getter
     private String pictureUrl;
-    private String ownerId; // Assuming ownerId is a reference to the User
-    private List<String> categoryIds; // References to Categories
+    @Getter
+    private String ownerId;
+    @Getter
+    private List<String> categoryIds; 
 
 
     public Rental(String title, String description, double pricePerDay, boolean availability, String location,String pictureUrl, String ownerId, List<String> categoryIds) {
