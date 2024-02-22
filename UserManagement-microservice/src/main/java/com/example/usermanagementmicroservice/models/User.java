5812717cd2c2fbe8895fd04bb1b08603dff030d2
RihabAitBahessou <117985681+rihabab@users.noreply.github.com>
2024-02-22 16:34:14 +0100

    commiting

--- UserManagement-microservice/src/main/java/com/example/usermanagementmicroservice/models/User.java
+++ UserManagement-microservice/src/main/java/com/example/usermanagementmicroservice/models/User.java
@@ -1,9 +1,12 @@
 package com.example.usermanagementmicroservice.models;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-
+@Data
+@NoArgsConstructor
 @Document(collection = "users")
 public class User {
     @Id
@@ -15,35 +18,5 @@ public class User {
 
     private String password;
 
-    public User() {}
 
-    public User(String username, String email, String password) {
-        this.username = username;
-        this.email = email;
-        this.password = password;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
