88c5bfcea8fca4a2175a742fdae3719042a98447
RihabAitBahessou <117985681+rihabab@users.noreply.github.com>
2024-02-24 23:16:51 +0100

    commi jwtt

--- UserManagement-microservice/src/main/java/com/example/usermanagementmicroservice/controllers/UserController.java
+++ UserManagement-microservice/src/main/java/com/example/usermanagementmicroservice/controllers/UserController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
@@ -44,3 +43,4 @@ public class UserController {
         userService.deleteUser(id);
     }
 }
+
