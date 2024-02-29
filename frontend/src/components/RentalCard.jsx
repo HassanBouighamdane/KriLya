981709e282456ecf394ed47a260e22dae9675599
HassanBouighamdane <hassanbouighamdane2002@gmail.com>
2024-02-29 23:16:13 +0100

    Resolve RentalCard.jsx problem

--- frontend/src/components/RentalCard.jsx
+++ frontend/src/components/RentalCard.jsx
@@ -55,43 +55,15 @@ export default function RentalCard({id,title, description, images, pricePerDay, 
                     <Typography color="red" className="font-medium">
                         {pricePerDay}$ per day
                     </Typography>
-                </div>
-                <Typography color="gray">
-                    {description}
-                </Typography>
-                <Typography color="red" className="font-medium">
-                    {pricePerDay}
-                </Typography>
-                <div className="group mt-8 inline-flex flex-wrap items-center gap-3">
-                    <Tooltip content="2 bedrooms">
-            <span
-                className="cursor-pointer rounded-full border border-gray-900/5 bg-gray-900/5 p-3 text-gray-900 transition-colors hover:border-gray-900/10 hover:bg-gray-900/10 hover:!opacity-100 group-hover:opacity-70">
-              <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  className="h-5 w-5"
-              >
-                <path d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z" />
-                <path d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z" />
-              </svg>
-            </span>
-                    </Tooltip>
-                    <Tooltip content="And +20 more">
-            <span className="cursor-pointer rounded-full border border-gray-900/5 bg-gray-900/5 p-3 text-gray-900 transition-colors hover:border-gray-900/10 hover:bg-gray-900/10 hover:!opacity-100 group-hover:opacity-70">
-              +20
-            </span>
-                    </Tooltip>
-                    <CardBody/>
-                </div>
-                
-            
-            <CardFooter className="p-3 bg-blue-900 " >
-                <Button className='bg-blue-900' size="lg" fullWidth={true} >
+                </CardBody>
+            </div>
+
+            <CardFooter className="pt-3 flex justify-between mt-3 space-x-4 ">
+             <Button className='bg-blue-900' size="lg" fullWidth={true} >
                     Book
                 </Button>
                 <Link className="block w-full" to={`/items/${id}/details`}>
-                <Button onClick={handleDetailsClick} className="pt-3 bg-blue-900 rounded-none" fullWidth={true} size="lg" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', padding: '10px' }}>
+                <Button onClick={handleDetailsClick} className="pt-3 bg-gray-600 " fullWidth={true} size="lg" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', padding: '10px' }}>
                     See more 
                 </Button>
                 </Link>
