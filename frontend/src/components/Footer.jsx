45c54fb83c20a4d3f3976f0a944f334c6ff62214
mehdiboutoumit <mehdiboutoumit03@gmail.com>
2024-03-01 00:22:32 +0100

    sql connection

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,16 +4,20 @@ function Footer(){
       <div className="max-w-screen-xl mx-auto px-4 grid grid-cols-1 md:grid-cols-2 gap-4">
         
         <div className="md:order-1">
-        <h4 className="text-center md:text-left">Contact</h4>
-          <p className="text-center md:text-left">Right Section</p>
+        <h4 className="text-center md:text-left"></h4>
+          <p className="text-center md:text-left">KriLya is a dynamic web application that serves as a versatile
+rental marketplace, catering to the needs of individuals seeking to both rent out and
+borrow a wide array of items. The platform offers users a convenient and interactive
+space to engage in peer-to-peer renting, facilitating transactions and fostering a sense
+of community collaboration.</p>
         </div>
 
         
         <div className="md:order-2">
         <h4 className="text-center md:text-left">Contact</h4>
-          <p className="text-center md:text-left">Left Section</p>
+          <p className="text-center md:text-left"></p>
           <h4 className="text-center md:text-left">Adress</h4>
-          <p className="text-center md:text-left">Left Section</p>
+          <p className="text-center md:text-left"></p>
         
         </div>
       </div>
