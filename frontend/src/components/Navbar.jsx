583a0c6a0f0763b9140347e4e3730b022dea36eb
mehdiboutoumit <mehdiboutoumit03@gmail.com>
2024-03-01 09:31:40 +0100

    update nav

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -66,23 +66,7 @@ function Navbar(){
               </li>
               }
 
-              {shouldDisplayButton() && (
-                    <li>
-                        <button type="button" className="w-50 flex items-center text-center px-5 py-2.5 font-medium tracking-wide text-white capitalize bg-black rounded-md hover:bg-gray-900 focus:outline-none transition duration-300 transform active:scale-95 ease-in-out">
-                            <svg xmlns="http://www.w3.org/2000/svg" enableBackground="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#FFFFFF">
-                                <g>
-                                    <rect fill="none" height="24" width="24"></rect>
-                                </g>
-                                <g>
-                                    <g>
-                                        <path d="M19,13h-6v6h-2v-6H5v-2h6V5h2v6h6V13z"></path>
-                                    </g>
-                                </g>
-                            </svg>
-                            <span className="pl-2 mx-1">Rent Out An Item</span>
-                        </button>
-                    </li>
-                )}
+             
               
             </ul>
           </div>
