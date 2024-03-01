993dc34ae498811179e4e449422e9365e8f8f8e9
mehdiboutoumit <mehdiboutoumit03@gmail.com>
2024-03-01 18:50:43 +0100

    useFetch custom hook

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import avatar from '../images/avatar.jpg';
 import RentalCard from '../components/RentalCard';
 import Review from '../components/Review';
 import { MdInsights } from "react-icons/md";
 import { IoCartSharp } from "react-icons/io5";
+import useFetch from '../hooks/useFetch';
+import API from '../services/UserManagementApi';
+
 
 function Profile() {
     const User = {firstname: "Jose", lastname: "Morinho", address: "12 rue xcvc rabat", phone: "0678585858",
      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus omnis, reiciendis aperiam numquam nam porro unde, iusto iste animi earum enim sapiente facere! Excepturi neque in eius expedita adipisci? Odit?"
     ,gender: "Male", email: "mail@gm.com", createdAt: "", status: "Active", rating: "4.89", responseRate: "1h"
     }
+
+    const {data, loading, error} = useFetch(API.PROFILE.GET_ALL);
+
+
+
     const reviews = "";
 
     
@@ -58,7 +66,7 @@ function Profile() {
                                 <span className="text-blue-900">
                                     <svg className="h-5 fill-current" xmlns="http://www.w3.org/2000/svg" fill="none"
                                         viewBox="0 0 24 24" stroke="currentColor">
-                                        <path stroke-linecap="round" stroke-linejoin="round" strokeWidth="2"
+                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                                             d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
                                     </svg>
                                 </span>
