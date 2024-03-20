984578bc3070c947dcdc532aa046d8ca4b3fa66f
RihabAitBahessou <117985681+rihabab@users.noreply.github.com>
2024-03-20 23:53:25 +0000

    gateway

--- UserManagement-microservice/src/main/java/com/example/usermanagementmicroservice/repository/UserRepository.java
+++ UserManagement-microservice/src/main/java/com/example/usermanagementmicroservice/repository/UserRepository.java
@@ -10,4 +10,7 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User,String> {
     User findByEmail(String email);
+    boolean existsByEmail(String email);
+
+    boolean existsByUsername(String username);
 }
